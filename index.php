73c1a8ff3ce13406c2ca498d0394deed37baf5d1
Eric Sanders <easander@easander.com>
2014-08-04 22:32:54 -0500

    Added logic to read the csv file and put an adjective before a noun

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <!DOCTYPE html>
+<?php
+
+  //gonna be ugly for the first cut, don't judge
+  define('CSV_WORD', 0);
+  define('CSV_IS_NOUN', 1);
+  define('CSV_IS_ADJECTIVE', 2);
+ 
+  $nouns = array();
+  $adjectives = array();
+
+
+  if (($handle = fopen("../words.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+      if ($data[CSV_IS_NOUN] == 'is_noun') {
+        continue;
+      }
+
+      if ($data[CSV_IS_NOUN] == 'TRUE') {
+        $nouns[] = $data[CSV_WORD];
+      }
+
+      if ($data[CSV_IS_ADJECTIVE] == 'TRUE') {
+        $adjectives[] = $data[CSV_WORD];
+      }
+    }
+    fclose($handle);
+  }
+
+?>
 <html lang="en">
   <head>
     <title>Metal band name generator ϟ metalband.me</title>
@@ -20,10 +49,10 @@
     </h2>
 
     <h3>
-      Miniscule Zombie
-    </h3>
+      <?= $adjectives[rand(0, count($adjectives) - 1)] ?> <?= $nouns[rand(0, count($nouns) - 1)] ?>
+    </h3>    
 
-    <form>
+    <form action="/">
       <button type="submit">
         <i class="fa fa-bolt"></i>
         <span>
